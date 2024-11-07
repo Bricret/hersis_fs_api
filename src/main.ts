0790fadaf9d42202d46605702f7f2d7bb0e972fe
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-11-06 22:46:14 -0600

    Agregar configuración inicial de NestJS con TypeScript, ESLint, Prettier y Docker

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+   );
+
+   app.setGlobalPrefix('apiV1');
+
+   app.enableCors();
+
+   await app.listen(process.env.PORT || 3000);
+   logger.log(`Application listening on port ${process.env.PORT}`);
+}
+bootstrap();
